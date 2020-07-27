5376bc46b4bed85ba72d9964e89b54e9fcd40ad6
daulet2030 <daulet2030@gmail.com>
2020-07-27 20:11:23 +0600

    firefox Opener

--- src/test/java/DriverManagerTest.java
+++ src/test/java/DriverManagerTest.java
@@ -1,6 +1,7 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class DriverManagerTest {
@@ -10,5 +11,12 @@ public class DriverManagerTest {
         WebDriverManager.chromedriver().setup();
         WebDriver chrome = new ChromeDriver();
         chrome.get("https://facebook.com");
+    }
+
+    @Test
+    public void firefoxOpener() {
+        WebDriverManager.firefoxdriver().setup();
+        WebDriver firefox = new FirefoxDriver();
+        firefox.get("https://facebook.com");
     }
 }
